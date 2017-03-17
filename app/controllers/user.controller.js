c0e1e02eda19d292c4523cc3d16ed4ab52bddd48
ben <b.roullet@skilvioo.com>
2017-03-17 09:15:48 +0100

    :construction: Refacto of error handler

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -8,14 +8,14 @@ exports.getAllUser = function(req, res){
       res.json(users);
     },
     function(error){
-      console.log(error);
+      errorHandler(res, "Aucuns utilisateur trouvé");
     })
 };
 
 exports.findOneById = function(req, res){
   User.findById(req.params.id ,function(err, user) {
       if(err) {
-        res.json({ success: false, message: 'Impossible de trouver cet utilisateur.'})
+        errorHandler(res, 'Impossible de trouver cet utilisateur.');
       } else {
         console.log("user", user);
         res.json(user);
@@ -26,21 +26,36 @@ exports.findOneById = function(req, res){
 
 exports.createUser = function(req, res) {
   if(!req.body.firstname || !req.body.lastname) {
-    res.json({ success: false, message: "Il manque un paramètre pour compléter la creation de l'utilisateur"})
+    errorHandler(res, "Il manque un paramètre pour compléter la creation de l'utilisateur");
   } else {
     User.findOne({firstname: req.body.firstname, lastname: req.body.lastname}, function(err, user){
       if(user) {
-        res.json({ success: false, message: 'Un utilisateur similaire existe déjà'});
+        errorHandler(res, 'Un utilisateur similaire existe déjà');
       } else {
         var newUser = new User({firstname: req.body.firstname, lastname: req.body.lastname});
         newUser.save(function(err){
           if(err) {
-            res.json({ success: false, message: "L'utilisateur n'a pas pu être créé"});
+            errorHandler(res, "L'utilisateur n'a pas pu être créé");
           } else {
-            res.json({ success: true, message: 'Utilisateur enregistré'})
+            res.json({ success: true, message: 'Utilisateur enregistré'});
           }
         });
       }
     });
   }
+};
+
+exports.deleteUser = function(req, res) {
+  User.deleteOne({ _id: req.body.id }, function(err) {
+    if (err) {
+      errorHandler(res, "Impossible de supprimer cet utilisateur");
+    } else {
+      res.json({success: true, message: "Utilsateur supprimé"});
+    }
+
+  })
+};
+
+errorHandler = function(res, errorMessage) {
+  res.json({ success: false, message: errorMessage})
 }
