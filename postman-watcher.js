e3f982b6f5286c666ba018873e535720e807f244
ben <b.roullet@skilvioo.com>
2017-03-20 13:55:58 +0100

    :memo: Add watcher for update readme with postman collection

---
+++ postman-watcher.js
@@ -0,0 +1,36 @@
+var chokidar = require('chokidar');
+var fs = require('fs');
+
+var watcher = chokidar.watch('./postman-collections/*json', {
+  persistent: true
+});
+
+var populateReadme = function(data) {
+  var content = "";
+  content += '# ' + data.info.name + ':\n\n';
+  content += '## API endpoints list:\n';
+  for(var i=0; i < data.item.length ; i++) {
+    content += '* **' + data.item[i].name + "**\n";
+    if(data.item[i].response.length > 0) {
+      content += '    * Output exemple:\n';
+      content += '```\n' + data.item[i].response[0].body + '\n```\n';
+    }
+    content += 'lancer le serveur: `node server.js`\n';
+    content += 'lancer le watcher pour la mise à jour de la doc via postman: `node postman-watcher.js`';
+  }
+  console.log(content);
+  fs.writeFileSync('README.md', content);
+}
+
+var fileChanged = function(path) {
+  console.log(`File ${__dirname}${path} Changed`);
+  fs.readFile(__dirname + '/' + path, function(err, data) {
+    if(err) {
+      console.log(err);
+    } else {
+      populateReadme(JSON.parse(data));
+    }
+  })
+}
+
+watcher.on('change', path => fileChanged(path));
