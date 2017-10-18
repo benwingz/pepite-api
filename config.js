7eaf0b9575e00468edc425774f83d85903940705
Ben <b.roullet@skilvioo.com>
2017-10-18 11:13:45 +0200

    :wrench: Prepare heroku deploy

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   'PROD': {
       'secret': 'jwtsecretdev',
-      'database': "localhost:27017/local"
+      'database': 'mongodb://pepite:pepite2017@ds123695.mlab.com:23695/heroku_lmdf0553'
   }
 
 };
